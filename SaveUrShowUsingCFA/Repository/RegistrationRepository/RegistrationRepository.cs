dfab84addae25d2848af0adb2fcf6953cbb1ac96
Jaswanth225 <86094274+Jaswanth225@users.noreply.github.com>
2023-03-16 10:49:13 +0530

    RegistrationRepository.cs

--- SaveUrShowUsingCFA/Repository/RegistrationRepository/RegistrationRepository.cs
+++ SaveUrShowUsingCFA/Repository/RegistrationRepository/RegistrationRepository.cs
@@ -22,14 +22,42 @@ namespace SaveUrShowUsingCFA.Repository.RegistrationRepository
         }
         public async Task<ActionResult<IEnumerable<Registration>>> GetRegistration()
         {
-            //_logger.LogInformation["Getting all the users successfully."];
+            _logger.LogInformation["Getting all the users successfully."];
             return await _context.Registration.ToListAsync();
             //throw new NotImplementedException();
+        }
+        
+        public async Task<ActionResult<Registration>> GetRegistration(int id)
+        {
+            var registration = await _context.Registration.FindAsync(id);
+
+            return registration;
         }
 
         public async Task<ActionResult<Registration>> GetRegistration(string email, string password)
         {
             var registration = await _context.Registration.FirstOrDefaultAsync(record => record.email == email && record.password == password); 
+            return registration;
+        }
+         public async Task<ActionResult<Registration>> PostRegistration(Registration registration)
+        {
+            _context.Registration.Add(registration);
+            await _context.SaveChangesAsync();
+            return registration;
+        }
+
+        public async Task<ActionResult<Registration>> PutRegistration(int id, Registration registration)
+        {
+            _context.Entry(registration).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return registration;
+        }
+
+        public async Task<ActionResult<Registration>> DeleteRegistration(int id)
+        {
+            var registration = await _context.Registration.FindAsync(id);
+            _context.Registration.Remove(registration);
+            await _context.SaveChangesAsync();
             return registration;
         }
     }
